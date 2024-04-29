f4004e99e57a4585c064c8c30d2707c1f50e5442
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-04-29 09:15:00 +0545

    update route files and path in server file

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 
-import authRoute from "./routes/auth.routes.js";
-import userRoute from "./routes/user.routes.js";
-import postRoute from "./routes/post.routes.js"
+import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js"
 
 import connectMongo from "./db/connectMongoDB.js";
 
