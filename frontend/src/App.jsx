15cbc02b26a5ff22c1181f1300315f95dfdb6912
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-05-03 08:19:50 +0545

    configured dark theme in daisyui, created  signup page and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import { Routes } from "react-router-dom"
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LogInPage from "./pages/auth/login/LogInPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
 
 function App() {
-
   return (
     <>
-    <div className="flex max-w-6xl mx-auto">
-       
-    </div>
-
+      <div className="">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LogInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,23 @@
-import daisyui from 'daisyui';
-import daisyUITheme from 'daisyui/src/theming/themeDefaults'
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: [
-      "light",
-      {
-        black: {
-          ...daisyUITheme["black"],
-          primary: "rgb(29,155, 240)",
-          secondary: "rgb(24,24,24)",
-        },
-      },
-    ],
-  },
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
+
+	daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(29, 155, 240)",
+					secondary: "rgb(24, 24, 24)",
+				},
+			},
+		],
+	},
 };
