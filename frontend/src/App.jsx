21978c69efb5a5cd1bc612fa2a637bfbf4f0025a
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-05-04 08:26:36 +0545

    profile page and notification page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,22 @@ import LogInPage from "./pages/auth/login/LogInPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
     <>
       <div className="flex max-w-6xl mx-auto">
-        <Sidebar/>
+        <Sidebar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LogInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/notifications" element={<NotificationPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
         </Routes>
-        <RightPanel/>
+        <RightPanel />
       </div>
     </>
   );
