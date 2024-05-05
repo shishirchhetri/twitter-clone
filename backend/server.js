c574ed64a37351a9938181e27d081ab9aefeceab
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-05-05 15:18:35 +0545

    implemented create new post and make max-file size to 5mb for image uploads

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import { v2 as cloudinary } from "cloudinary";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
 
-import authRoute from "./routes/auth.route.js";
-import userRoute from "./routes/user.route.js";
-import postRoute from "./routes/post.route.js";
-import notificationRoute from './routes/notification.route.js'
+import authRoute from './routes/auth.route.js';
+import userRoute from './routes/user.route.js';
+import postRoute from './routes/post.route.js';
+import notificationRoute from './routes/notification.route.js';
 
-import connectMongo from "./db/connectMongoDB.js";
+import connectMongo from './db/connectMongoDB.js';
 
 dotenv.config();
 //cloudinary image update config
@@ -23,15 +23,16 @@ const PORT = process.env.PORT;
 //for parsing jwt cookie token
 app.use(cookieParser());
 //middleware to parse data from frontend
-app.use(express.json());
+//providing 5mb by overwriting 100kb default (for image upload)
+app.use(express.json({ limit: '5mb' }));
 //parse form data (url encoded)
 app.use(express.urlencoded({ extended: true }));
 
 //routes
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/notifications", notificationRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
+app.use('/api/notifications', notificationRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -1,24 +1,68 @@
-import { CiImageOn } from "react-icons/ci";
-import { BsEmojiSmileFill } from "react-icons/bs";
-import { useRef, useState } from "react";
-import { IoCloseSharp } from "react-icons/io5";
+import { CiImageOn } from 'react-icons/ci';
+import { BsEmojiSmileFill } from 'react-icons/bs';
+import { useRef, useState } from 'react';
+import { IoCloseSharp } from 'react-icons/io5';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 
 const CreatePost = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
   const [img, setImg] = useState(null);
 
   const imgRef = useRef(null);
 
-  const isPending = false;
-  const isError = false;
+  //getting the loggedin user details
+  const { data: authUser } = useQuery({
+    queryKey: ['authUser'],
+  });
 
-  const data = {
-    profileImg: "/avatars/boy2.png",
-  };
+  //for invalidationg query, updating cache
+  const queryClient = useQueryClient();
+
+  // react-query mutation function for creating post
+  const {
+    mutate: createPost,
+    isPending,
+    isError,
+    error,
+  } = useMutation({
+    mutationFn: async ({ text, img }) => {
+      try {
+        const res = await fetch('/api/posts/create', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ text, img }),
+        });
+        const data = await res.json({ text, img });
+
+        if (!res.ok) {
+          throw new Error(data.error || 'failed to create new post!');
+        }
+
+        console.log(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      toast.success('created a new post successfully!');
+      //revalidate the posts so new posts are shown as well
+      queryClient.invalidateQueries({
+        queryKey: ['posts'],
+      });
+      //reset the state:
+      setText('');
+      setImg(null);
+    },
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert("Post created successfully");
+    createPost({ text, img });
   };
 
   const handleImgChange = (e) => {
@@ -33,23 +77,23 @@ const CreatePost = () => {
   };
 
   return (
-    <div className="flex p-4 items-start gap-4 border-b border-gray-700">
-      <div className="avatar">
-        <div className="w-8 rounded-full">
-          <img src={data.profileImg || "/avatar-placeholder.png"} />
+    <div className='flex p-4 items-start gap-4 border-b border-gray-700'>
+      <div className='avatar'>
+        <div className='w-8 rounded-full'>
+          <img src={authUser.profileImg || '/avatar-placeholder.png'} />
         </div>
       </div>
-      <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
+      <form className='flex flex-col gap-2 w-full' onSubmit={handleSubmit}>
         <textarea
-          className="textarea w-full p-0 text-lg resize-none border-none focus:outline-none  border-gray-800"
-          placeholder="What is happening?!"
+          className='textarea w-full p-0 text-lg resize-none border-none focus:outline-none  border-gray-800'
+          placeholder='What is happening?!'
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
         {img && (
-          <div className="relative w-72 mx-auto">
+          <div className='relative w-72 mx-auto'>
             <IoCloseSharp
-              className="absolute top-0 right-0 text-white bg-gray-800 rounded-full w-5 h-5 cursor-pointer"
+              className='absolute top-0 right-0 text-white bg-gray-800 rounded-full w-5 h-5 cursor-pointer'
               onClick={() => {
                 setImg(null);
                 imgRef.current.value = null;
@@ -57,25 +101,31 @@ const CreatePost = () => {
             />
             <img
               src={img}
-              className="w-full mx-auto h-72 object-contain rounded"
+              className='w-full mx-auto h-72 object-contain rounded'
             />
           </div>
         )}
 
-        <div className="flex justify-between border-t py-2 border-t-gray-700">
-          <div className="flex gap-1 items-center">
+        <div className='flex justify-between border-t py-2 border-t-gray-700'>
+          <div className='flex gap-1 items-center'>
             <CiImageOn
-              className="fill-primary w-6 h-6 cursor-pointer"
+              className='fill-primary w-6 h-6 cursor-pointer'
               onClick={() => imgRef.current.click()}
             />
-            <BsEmojiSmileFill className="fill-primary w-5 h-5 cursor-pointer" />
+            <BsEmojiSmileFill className='fill-primary w-5 h-5 cursor-pointer' />
           </div>
-          <input type="file" accept="image/*" hidden ref={imgRef} onChange={handleImgChange} />
-          <button className="btn btn-primary rounded-full btn-sm text-white px-4">
-            {isPending ? "Posting..." : "Post"}
+          <input
+            type='file'
+            accept='image/*'
+            hidden
+            ref={imgRef}
+            onChange={handleImgChange}
+          />
+          <button className='btn btn-primary rounded-full btn-sm text-white px-4'>
+            {isPending ? 'Posting...' : 'Post'}
           </button>
         </div>
-        {isError && <div className="text-red-500">Something went wrong</div>}
+        {isError && <div className='text-red-500'>{error.message}</div>}
       </form>
     </div>
   );
